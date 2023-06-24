9b62e806567bd4e493b808c20ae7da76db991d37
pieromarini <piero.marini21@gmail.com>
2023-06-24 12:11:53 -0500

    implemented UI camera follow

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Camera followCamera;
+    public float distance = 3.0F;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        Vector3 targetPosition = followCamera.transform.TransformPoint(new Vector3(0, -0.15f, distance));
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        var lookAtPos = new Vector3(followCamera.transform.position.x, transform.position.y, followCamera.transform.position.z);
+        transform.LookAt(lookAtPos);
+        transform.forward *= -1;
+    }
+}
