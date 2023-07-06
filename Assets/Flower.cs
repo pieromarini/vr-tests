75506cb34437434916e1e66328ed93f3e52c18ac
Piero Marini <piero.marini21@gmail.com>
2023-07-06 18:50:53 -0500

    ScriptableObjects to store flower data. show tooltip when hovering pots

---
+++ Assets/Flower.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    public FlowerData data;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public string getFlowerName()
+    {
+        return data.name;
+    }
+
+    public float getWaterNeeds()
+    {
+        return data.water;
+    }
+
+    public float getFertilizerNeeds()
+    {
+        return data.fertilizer;
+    }
+}
